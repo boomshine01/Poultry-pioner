dc207bd4da3db413ab6e28c5b2c648ce28c75c32
boomshine01 <mamecdiop1@gmail.com>
2025-04-06 20:30:20 +0200

    Replace all with cleaned local version

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-
+const repoName = "Poultry-pioner";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Poultry-pioner/",
   server: {
     host: "::",
     port: 8080,
